ef28cce90ae1b0a95055a7256cc0307fa76f150e
Julius Duff <8215455@ntc.edu.ph>
2024-06-25 20:49:58 +0800

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html, js}"],
+  content: ["./*.{html, js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
